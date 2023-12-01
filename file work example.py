bf54cc0cfd9d3630b1da711431c976379522721b
Abdikassym <79089741+Abdikassym@users.noreply.github.com>
2023-12-01 23:30:46 +0600

    Delete file work example.py

--- file work example.py
+++
@@ -1,7 +0,0 @@
-# Absolute file path
-# with open("/Users/Abdi/Desktop/doc.txt", mode='r') as file:
-#     print(file.read())
-
-# using relative file path
-with open("../../../Desktop/doc.txt") as file:
-    print(file.read())
